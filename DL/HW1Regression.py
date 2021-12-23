8cf902f2d921fb881e75a92522acae3ee37547b9
PeterWright <peterwrighten@gmail.com>
2021-12-24 01:13:22 +0900

    update: init HW1

---
+++ DL/HW1Regression.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+import numpy as np
+import csv
+import os
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+myseed = 42069  # set a random seed for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
